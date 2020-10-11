37c52f89944d87d1766f9df36cd581dd53c145cd
Vinicius Batista Barbosa <vinicius.batista.barbosa34@gmail.com>
2020-10-11 12:25:40 -0300

    IOC configurada :fire:

--- aspnetDDD.Infrastructure/CrossCutting/IOC/ConfigurationIOC.cs
+++ aspnetDDD.Infrastructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -1,19 +1,31 @@
 using aspnetDDD.Application;
 using aspnetDDD.Application.Interfaces;
+using aspnetDDD.Application.Interfaces.Mappers;
+using aspnetDDD.Application.Mappers;
+using aspnetDDD.Domain.Core.Interfaces.Repositorys;
 using aspnetDDD.Domain.Core.Interfaces.Services;
+using aspnetDDD.Domain.Services;
+using aspnetDDD.Infrastructure.Data.Repositorys;
 using Autofac;
 
 namespace aspnetDDD.Infrastructure.CrossCutting.IOC
 {
     public class ConfigurationIOC
     {
-        public static void Loid(ContainerBuilder builder)
+        public static void Load(ContainerBuilder builder)
         {
             #region IOC
             builder.RegisterType<ApplicationServiceCliente>().As<IApplicationServiceCliente>();
             builder.RegisterType<ApplicationServiceProduto>().As<IApplicationServiceProduto>();
 
-            // builder.RegisterType<ServiceCliente>().As<IServiceCliente>();
+            builder.RegisterType<ServiceCliente>().As<IServiceCliente>();
+            builder.RegisterType<ServiceProduto>().As<IServiceProduto>();
+
+            builder.RegisterType<RepositoryCliente>().As<IRepositoryCliente>();
+            builder.RegisterType<RepositoryProduto>().As<IRepositoryProduto>();
+
+            builder.RegisterType<MapperCliente>().As<IMapperCliente>();
+            builder.RegisterType<MapperProduto>().As<IMapperProduto>();
 
             #endregion
         }
---
+++ aspnetDDD.Infrastructure/CrossCutting/IOC/ModuleIOC.cs
@@ -0,0 +1,12 @@
+using Autofac;
+
+namespace aspnetDDD.Infrastructure.CrossCutting.IOC
+{
+    public class ModuleIOC : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIOC.Load(builder);
+        }
+    }
+}
